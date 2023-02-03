408bf453f28abcd370b4f4448f82b20fea6ba8ba
Tim Stack <tstack43215@yahoo.com>
2023-02-03 14:18:19 -0600

    Initial Commit

---
+++ SeriesTEst/SeriesTEstApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriesTEstApp.swift
+//  SeriesTEst
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SeriesTEstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
